6542f8f00e4dd8c25d970036fd9637c07044d562
Amber Crawford <ammcrawford@gmail.com>
2015-11-03 14:15:02 -0700

    Create user test passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
-  helper_method :most_recent_song
+  # helper_method :most_recent_song#, :current_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def most_recent_song
-    Song.find(session[:most_recent_song_id])
+  # def most_recent_song
+  #   Song.find(session[:most_recent_song_id])
+  # end
+
+  helper_method :current_user
+
+
+  def current_user
+    @user || User.find(session[:user_id]) if session[:user_id]
   end
 end
